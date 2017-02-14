7f00ef49e19f3fc1e73e138d13b6667b790a2b07
JoramWilander <jwawilander@gmail.com>
2017-02-13 20:39:18 -0500

    Implement POST /hooks/incoming endpoint for APIv4

--- model/client4.go
+++ model/client4.go
@@ -112,6 +112,10 @@ func (c *Client4) GetPostRoute(postId string) string {
 	return fmt.Sprintf(c.GetPostsRoute()+"/%v", postId)
 }
 
+func (c *Client4) GetIncomingWebhooksRoute() string {
+	return fmt.Sprintf("/hooks/incoming")
+}
+
 func (c *Client4) DoApiGet(url string, etag string) (*http.Response, *AppError) {
 	return c.DoApiRequest(http.MethodGet, url, "", etag)
 }
@@ -562,3 +566,15 @@ func (c *Client4) GetPostsForChannel(channelId string, page, perPage int, etag s
 
 // Files Section
 // to be filled in..
+
+// Webhooks Section
+
+// CreateIncomingWebhook creates an incoming webhook for a channel.
+func (c *Client4) CreateIncomingWebhook(hook *IncomingWebhook) (*IncomingWebhook, *Response) {
+	if r, err := c.DoApiPost(c.GetIncomingWebhooksRoute(), hook.ToJson()); err != nil {
+		return nil, &Response{StatusCode: r.StatusCode, Error: err}
+	} else {
+		defer closeBody(r)
+		return IncomingWebhookFromJson(r.Body), BuildResponse(r)
+	}
+}
